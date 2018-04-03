b2a21845ef44edf27d9f0f417a4e0b6d44026e6a
Louis LeBohec <louisblebohec@gmail.com>
2018-04-03 11:13:01 -0600

    fixed React Nav

---
+++ Desktop/elixir-site_v3/src/Components/Location.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Location extends Component{
+    render(){
+        return(
+            <div className={"frosted main-content"}>
+                <h1>Here we are!!</h1>
+            </div>
+        )
+    }
+}
